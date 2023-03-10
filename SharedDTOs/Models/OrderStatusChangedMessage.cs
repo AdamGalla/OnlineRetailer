e0f67ea0d661e34b72f085851683102e690ce868
AdamGalla <ado.galla@gmail.com>
2023-03-10 10:05:52 +0100

    OrderStatusMessage

---
+++ SharedDTOs/Models/OrderStatusChangedMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedDTOs.Models;
+
+public class OrderStatusChangedMessage
+{
+    public int? CustomerId { get; set; }
+    public IList<OrderLineDto> OrderLines { get; set; }
+}
